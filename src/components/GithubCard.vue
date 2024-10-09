c003268732371feb52480c2a00c42e102cb09270
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-10-09 12:00:48 +0200

    fix

--- src/components/GithubCard.vue
+++ src/components/GithubCard.vue
@@ -12,7 +12,6 @@ const count = ref(0)
       <img :src="userToPrint.avatar_url" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">{{ userToPrint.login }}</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a :href="userToPrint.html_url" class="btn btn-primary">View Profile</a>
       </div>
     </div>
